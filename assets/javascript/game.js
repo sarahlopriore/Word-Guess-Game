a18f128ee65e4814f222a10332a142e418e3e731
unknown <sarah.lopriore@gmail.com>
2018-08-17 15:48:01 -0400

    basic html setup and establish letters array

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+
+// Establish an array for all possible letter guesses
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// Establish an array for all word guess options
+
+// When page is loaded Computer chooses a word for player to guess and displays blanks for number of letters in the word
+
+// Listen for key events for letters that are typed 
+
+// Check key guess against letters in the word
+
+// If letter is in the word then fill blanks appropriately
+
+// If letter is not in the word, number of guesses goes down
+
+// Wrong letter guessed gets added to the "letters already guessed"
+
+// If a letter was already guessed, do not let player guess that letter again 
+// (letter does not decrease number of guesses or display in the letters already guessed)
+
+// Block user from entering non-letter keys
+
+// When word is correctly guessed resey number of guesses
+
+// Reset letters already guessed
+
+// Add 1 to the number of wins
+
+// Change picture and text ?
+
+// Computer chooses a new word, and new blanks are displayed for each letter in the word
+
+// When word is not guessed before number of guesses reaches 0, reset number of guesses
+
+// Reset letters already guessed
+
+// Computer chooses a new word, and new blanks are displayed for each letter in the word
+
+    
+
